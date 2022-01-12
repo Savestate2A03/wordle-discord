a90001dadadff00bc2a4b27b432e50c473f7629d
Savestate2A03 <joseph.elkhouri+github@gmail.com>
2022-01-12 23:30:09 +0100

    add comments

--- extensions/wordle.py
+++ extensions/wordle.py
@@ -24,6 +24,7 @@ class Wordle(commands.Cog):
 		player  = user.id
 		if channel not in self.games:
 			await ctx.send('New wordle game started! Type `!join` to play.')
+			# game context per channel
 			self.games[channel] = {
 				'players': {},
 				'active': False,
@@ -36,6 +37,7 @@ class Wordle(commands.Cog):
 
 	@commands.command()
 	async def join(self, ctx, *args):
+		# allows a user to join a game
 		if ctx.channel.id not in self.games:
 			await ctx.send('No game is currently running! Type `!wordle` to start one.')
 			return
@@ -46,6 +48,7 @@ class Wordle(commands.Cog):
 			await ctx.send(f'The game has already started! Please wait until the next.')
 			return
 		if player not in game['players']:
+			# initial player context
 			game['players'][player] = {
 				'name': user.display_name,
 				'guesses': [],
@@ -58,6 +61,7 @@ class Wordle(commands.Cog):
 
 	@commands.command()
 	async def leave(self, ctx, *args):
+		# deletes player context from game.
 		channel = ctx.channel.id
 		if channel not in self.games:
 			await ctx.send(f'{user.mention} No game is currently running!')
@@ -66,6 +70,8 @@ class Wordle(commands.Cog):
 		user    = ctx.author
 		player  = user.id
 		if player in game['players']:
+			# all info for a player is contained in
+			# this context so it's fine to delete one mid-game.
 			game['players'].pop(player)
 			await ctx.send(f'{user.mention}  has left.')
 		else:
@@ -76,6 +82,9 @@ class Wordle(commands.Cog):
 
 	@commands.command()
 	async def stop(self, ctx, *args):
+		# stops the current game. currently not
+		# role limited. if someone is stopping 
+		# your game to troll, kick them (:
 		channel = ctx.channel.id
 		if channel not in self.games:
 			await ctx.send(f'{user.mention} No game is currently running!')
@@ -91,6 +100,7 @@ class Wordle(commands.Cog):
 
 	@commands.command()
 	async def start(self, ctx, *args):
+		# mark the game context as started.
 		channel = ctx.channel.id
 		user    = ctx.author
 		if channel not in self.games:
@@ -101,6 +111,7 @@ class Wordle(commands.Cog):
 			await ctx.send('A game has already started!')
 			return
 		if not game['players']:
+			# should never happen but idk
 			await ctx.send('At least 1 player is needed to play!')
 			return
 		game['word'] = random.choice(self.wordlist).lower()
@@ -109,20 +120,25 @@ class Wordle(commands.Cog):
 
 	@commands.command()
 	async def guess(self, ctx, *args):
+		# can probably be split up into multiple parts. oh well lol
 		channel = ctx.channel.id
 		user    = ctx.author
 		player  = user.id
+		# basic integrity checks
 		if channel not in self.games:
 			await ctx.send(f'{user.mention} No game is currently running! Type `!wordle` to start one.')
 			return
+		# first argument required, its the gussed word
 		if not args:
 			await ctx.send(f'{user.mention} Guess by typing in your guess spoilered (!guess ||like this||)')
 			return
+		# setting up some useful variables
 		guess         = args[0].lower()
 		game          = self.games[channel]
 		players       = game['players']
 		word          = game['word'].lower()
 		prev_messages = players[player]['prev_messages']
+		# context checking
 		if not game['active']:
 			await ctx.send(f'{user.mention} Wait until the game has started before guessing!')
 			return
@@ -134,11 +150,13 @@ class Wordle(commands.Cog):
 			await ctx.message.delete()
 			await ctx.send(f'{user.mention} You\'ve already solved the word! Wait for others to finish.')
 			return
+		# make sure guess is spoilered with regex matching
 		pattern = re.compile(r"\|\|(.+)\|\|")
 		if not pattern.match(guess):
 			await ctx.message.delete()
 			prev_messages.append(await ctx.send(f'{user.mention} Make sure to spoiler your guess!'))
 			return
+		# extract guess and do checks on it
 		guess = pattern.match(guess).group(1)
 		if len(guess) != len(word):
 			await ctx.message.delete()
@@ -148,25 +166,35 @@ class Wordle(commands.Cog):
 			await ctx.message.delete()
 			prev_messages.append(await ctx.send(f'{user.mention} Not a valid word!'))
 			return
+		# guess passes checks, add to player's word list
 		players[player]['guesses'].append(guess)
+		# then delete their guess for good measure
 		await ctx.message.delete()
+		# delete all pending messages for user as well to clean up board
 		for prev_message in prev_messages:
 			await prev_message.delete()
 			prev_messages.remove(prev_message)
+		# mark player's context as finished if guessed correctly
 		players[player]['finished'] = (guess == word)
+		# limit to 6 guesses as per official Wordle
 		if len(game['players'][player]['guesses']) >= 6:
 			players[player]['finished'] = True
+		# default message mentions user, add more past that
 		attached_message = user.mention
 		if guess == word:
 			attached_message += ' Congratulations on solving the word!'
 		elif players[player]['finished']:
 			attached_message += f' Sorry! You\'ve run out of guesses. The word was ||`{word}`||.'
+		# send a message containing the attached message and
+		# an embed generated by the Wordle embed generator.
 		prev_messages.append(await ctx.send(attached_message, embed=self.embed(word, game, player, user)))
+		# if all players are finished, wrap up and call the final embed.
 		all_finished = True
 		for player_i in players:
 			if not players[player_i]['finished']:
 				all_finished = False
 		if all_finished:
+			# delete any remaining pending messages
 			for player_i in players:
 				for prev_message in players[player_i]['prev_messages']:
 					await prev_message.delete()
@@ -188,36 +216,62 @@ class Wordle(commands.Cog):
 		guesses_text = ''
 		blocks_text  = ''
 		protected_letters = []
+		# loop through all 6 guessed words (if they exist)
 		for i in range(6):
+			# we modify the word as a part of the
+			# checking process, so we store it to
+			# a temporary variable that we modify
 			compare_word = word
 			if i >= len(guesses):
 				if not finished:
+					# only draw white squares if the player isn't finished
 					blocks_text += f'{(emoji_white * len(word))}\n'
 				continue
+			# get the current guess and add it to the guesses embed text
 			guess = guesses[i]
 			guesses_text += f'{i+1}: ||`{guess}`||\n'
-			# greens
+			# greens checking
 			for j in range(len(word)):
+				# if the current character is the same as the word...
 				if guess[j] == compare_word[j]:
+					# mark it on the keyboard
 					keyboard = keyboard.replace(f' {guess[j]}', f'>{guess[j]}')
+					# remove it from the word we're comparing to
 					compare_word = compare_word[:j] + ' ' + compare_word[j+1:]
+					# add it to the protected characters list to prevent
+					# it from being removed during the black square check
 					protected_letters.append(guess[j])
+					# and replace the character itself with a green square emoji
 					guess = guess[:j] + emoji_green + guess[j+1:]
 			# yellows
 			for j in range(len(word)):
+				# if the current character is in the word...
 				if guess[j] in compare_word:
+					# don't recall if this comparison is redunant now with the
+					# character replacement tech. too lazy to bug check it so
+					# i'm leaving it in. 
 					if guess[:(j+1)].count(guess[j]) <= compare_word.count(guess[j]):
+						# mark it on the keyboard
 						keyboard = keyboard.replace(f' {guess[j]}', f'>{guess[j]}')
+						# remove it from the word we're comparing to
 						compare_word = compare_word.replace(guess[j], ' ', 1)
+						# add it to the protected characters list to prevent
+						# it from being removed during the black square check
 						protected_letters.append(guess[j])
+						# and replace the character itself with a yellow square emoji
 						guess = guess[:j] + emoji_yellow + guess[j+1:]
 					else:
 						guess = guess[:j] + emoji_black + guess[j+1:]
 			# blacks
 			for j in range(len(word)):
+				# if the current character is in the word was not
+				# replaced by a green or yellow square emoji...
 				if guess[j] != emoji_green and guess[j] != emoji_yellow:
+					# AND it's not in the protected letters list...
 					if guess[j] not in protected_letters:
+						# remove it from the visible keyboard
 						keyboard = keyboard.replace(guess[j], f' ')
+					# regardless, replace it with a black square emoji in the guess
 					guess = guess[:j] + emoji_black + guess[j+1:]
 			blocks_text += f'{guess}\n'
 
@@ -233,6 +287,10 @@ class Wordle(commands.Cog):
 		return embed
 
 	def embed_final(self, word, game):
+		# this is pretty much a watered down version of the
+		# above function. read it if you need help understanding 
+		# what is going on.
+
 		emoji_white  = "\U00002B1C"
 		emoji_green  = "\U0001F7E9"
 		emoji_yellow = "\U0001F7E8"
